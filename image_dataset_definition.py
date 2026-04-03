5e15ce0d5642065d606da2db1dfb938bc1e6c45c
shivangi2002 <shivangi.gautam200211@gmail.com>
2026-04-03 16:20:53 +0530

    Add custom Dataset to load X-ray images with labels

---
+++ image_dataset_definition.py
@@ -0,0 +1,24 @@
+import os
+
+class XRayDatasetDefinition:
+    def __init__(self, dataset_path):
+        self.dataset_path = dataset_path
+       
+       # Create a mapping from class names to integer labels
+        self.class_to_label = {class_name: idx 
+            for idx, class_name in enumerate(sorted(os.listdir(dataset_path)))
+            }
+        
+        # Create a list of (image_path, label) tuples
+        self.samples = []
+        for class_name in self.class_to_label:
+            label = self.class_to_label[class_name]
+            class_path = os.path.join(self.dataset_path,class_name)
+            
+            for img_name in sorted(os.listdir(class_path)):
+                img_path = os.path.join(class_path, img_name)
+                self.samples.append((img_path, label))
+            
+    def __len__(self):
+        return len(self.samples)
+            
